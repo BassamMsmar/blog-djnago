546bd37f3ec01589ade3026562a6166ff1acddf1
Bassam Msmar <48624392+BassamMsmar@users.noreply.github.com>
2023-07-14 10:36:40 +0000

    add details template

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import posts
+from .views import posts, post_detail
 
 urlpatterns = [
-    path('', posts, name='posts' )
+    path('', posts, name='posts' ),
+    path('detail/<int:post_id>', post_detail, name='post_detail' ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,8 @@ from .models import Post
 def posts(request):
     posts = Post.objects.all()
     return render(request, 'posts/posts.html', {'posts':posts})
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'posts/post_detail.html', {'post':post})
